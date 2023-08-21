3fd8743699102c8f378d478661b1d9c1f6e0632d
Christopher Finster <finsternavy@gmail.com>
2023-08-20 21:47:16 -0500

    added highlighting to character matrix

--- src/views/Roster.vue
+++ src/views/Roster.vue
@@ -1,6 +1,8 @@
 <script setup>
 
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+
+const selectedClass = ref();
 
 const classes = {
     FIGHTER : [
@@ -8,41 +10,49 @@ const classes = {
             name : 'WEAPON MASTER',
             primary: 'FIGHTER',
             secondary: 'FIGHTER',
+            index: 0,
         }, 
         {
             name : 'DREADNOUGHT',
             primary: 'FIGHTER',
             secondary: 'TANK',
+            index: 1,
         }, 
         {
             name : 'SHADOWBLADE',
             primary: 'FIGHTER',
             secondary: 'ROGUE',
+            index: 2,
         },
         {
             name : 'HUNTER',
             primary: 'FIGHTER',
             secondary: 'RANGER',
+            index: 3,
         }, 
         {
             name : 'SPELLSWORD',
             primary: 'FIGHTER',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'BLADECALLER',
             primary: 'FIGHTER',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'HIGHSWORD',
             primary: 'FIGHTER',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'BLADEDANCER',
             primary: 'FIGHTER',
             secondary: 'BARD',
+            index: 7
         }
     ],
     TANK : [
@@ -50,41 +60,49 @@ const classes = {
             name : 'KNIGHT',
             primary: 'TANK',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'GUARDIAN',
             primary: 'TANK',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'NIGHTSHIELD',
             primary: 'TANK',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'WARDEN',
             primary: 'TANK',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'SPELLSHIELD',
             primary: 'TANK',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'KEEPER',
             primary: 'TANK',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'PALADIN',
             primary: 'TANK',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'ARGENT',
             primary: 'TANK',
             secondary: 'BARD',
+            index: 7
         }
     ],
     ROGUE : [
@@ -92,41 +110,49 @@ const classes = {
             name : 'DUELIST',
             primary: 'ROGUE',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'SHADOOW GUARDIAN',
             primary: 'ROGUE',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'ASSASSIN',
             primary: 'ROGUE',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'PREDATOR',
             primary: 'ROGUE',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'NIGHTSPELL',
             primary: 'ROGUE',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'SHADOW LORD',
             primary: 'ROGUE',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'CULTIST',
             primary: 'ROGUE',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'CHARLATAN',
             primary: 'ROGUE',
             secondary: 'BARD',
+            index: 7
         }
     ],
     RANGER : [
@@ -134,41 +160,49 @@ const classes = {
             name : 'STRIDER',
             primary: 'RANGER',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'SENTINEL',
             primary: 'RANGER',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'SCOUT',
             primary: 'RANGER',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'HAWKEYE',
             primary: 'RANGER',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'SCION',
             primary: 'RANGER',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'FALCONER',
             primary: 'RANGER',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'SOULBOW',
             primary: 'RANGER',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'BOWSINGER',
             primary: 'RANGER',
             secondary: 'BARD',
+            index: 7
         }
     ],
     MAGE : [
@@ -176,41 +210,49 @@ const classes = {
             name : 'BATTLE MAGE',
             primary: 'MAGE',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'SPELLSTONE',
             primary: 'MAGE',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'SHADOW CASTER',
             primary: 'MAGE',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'SPELLHUNTER',
             primary: 'MAGE',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'ARCHWIZARD',
             primary: 'MAGE',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'WARLOCK',
             primary: 'MAGE',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'ACOLYTE',
             primary: 'MAGE',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'SORCERER',
             primary: 'MAGE',
             secondary: 'BARD',
+            index: 7
         }
     ],
     SUMMONER : [
@@ -218,41 +260,49 @@ const classes = {
             name : 'WILD BLADE',
             primary: 'SUMMONER',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'BROOD WARDEN',
             primary: 'SUMMONER',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'SHADOWMANCER',
             primary: 'SUMMONER',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'BEASTMASTER',
             primary: 'SUMMONER',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'SPELLMANCER',
             primary: 'SUMMONER',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'CONJURER',
             primary: 'SUMMONER',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'NECROMANCER',
             primary: 'SUMMONER',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'ENCHANTER',
             primary: 'SUMMONER',
             secondary: 'BARD',
+            index: 7
         }
     ],
     CLERIC : [
@@ -260,41 +310,49 @@ const classes = {
             name : 'TEMPLAR',
             primary: 'CLERIC',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'APOSTLE',
             primary: 'CLERIC',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'SHADOW DISCIPLE',
             primary: 'CLERIC',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'PROTECTOR',
             primary: 'CLERIC',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'ORACLE',
             primary: 'CLERIC',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'SHAMAN',
             primary: 'CLERIC',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'HIGH PRIEST',
             primary: 'CLERIC',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'SCRYER',
             primary: 'CLERIC',
             secondary: 'BARD',
+            index: 7
         }
     ],
     BARD : [
@@ -302,41 +360,49 @@ const classes = {
             name : 'TELLSWORD',
             primary: 'BARD',
             secondary: 'FIGHTER',
+            index: 0
         }, 
         {
             name : 'SIREN',
             primary: 'BARD',
             secondary: 'TANK',
+            index: 1
         }, 
         {
             name : 'TRICKSTER',
             primary: 'BARD',
             secondary: 'ROGUE',
+            index: 2
         }, 
         {
             name : 'SONG WARDEN',
             primary: 'BARD',
             secondary: 'RANGER',
+            index: 3
         }, 
         {
             name : 'MAGICIAN',
             primary: 'BARD',
             secondary: 'MAGE',
+            index: 4
         }, 
         {
             name : 'SONGCALLER',
             primary: 'BARD',
             secondary: 'SUMMONER',
+            index: 5
         }, 
         {
             name : 'SOUL WEAVER',
             primary: 'BARD',
             secondary: 'CLERIC',
+            index: 6
         }, 
         {
             name : 'MINSTREL',
             primary: 'BARD',
             secondary: 'BARD',
+            index: 7
         }
     ],
 }
@@ -347,6 +413,7 @@ const headers = computed(() => {
 
 const selectClass = (subclass) => {
     console.log(subclass);
+    selectedClass.value = subclass;
 }
 </script>
 
@@ -362,7 +429,6 @@ const selectClass = (subclass) => {
 
 .title {
     color: orange;
-    padding-left: 0px;
 }
 
 .small-text {
@@ -371,6 +437,20 @@ const selectClass = (subclass) => {
 
 .true-class {
     background-color: rgba(255, 165, 0, .1);
+}
+
+.selected-types {
+    background-color: rgba(255, 165, 0, .3);
+    /* color: black; */
+}
+
+.selected {
+    background-color: orange;
+    color: black;
+}
+
+.between {
+    background-color: rgba(255, 255, 255, .2);
 }
 </style>
 
@@ -384,17 +464,26 @@ const selectClass = (subclass) => {
                 <thead>
                     <tr>
                         <td></td>
-                        <td v-for="header in headers" class="uk-text-center title">{{ header }}</td>
+                        <td v-for="header in headers" :class="{'uk-text-center title' : header, 'selected-types' : selectedClass && header == selectedClass.secondary}">{{ header }}</td>
                     </tr>
                 </thead>
                 <tbody class="uk-padding">
                     <tr class="uk-text-center" v-for="key, index1 in Object.keys(classes)">
-                        <td class="uk-text-middle title">{{ key }}</td>
+                        <td :class="{
+                            'uk-text-middle title uk-padding-remove-left' : key, 
+                            'selected-types' : selectedClass && key == selectedClass.primary}">{{ key }}</td>
                         <td v-for="subclass, index2 in classes[key]" 
                             :key="subclass.name + '_tile'"
                             @click="selectClass(subclass)"
-                            :class="{'cell uk-text-center uk-text-middle small-text' : {}, 'true-class' : index1 == index2}">
+                            :class="{
+                                'cell uk-text-center uk-text-middle small-text' : subclass, 
+                                'true-class' : index1 == index2,
+                                'between' : selectedClass && selectedClass.primary == key && index2 < selectedClass.index || 
+                                    selectedClass && selectedClass.secondary == subclass.secondary && headers.indexOf(selectedClass.primary) > headers.indexOf(subclass.primary),
+                                    'selected' : selectedClass && subclass.name == selectedClass.name
+                                }">
                             {{ subclass.name }}
+                            {{ index1 }} | {{ index2 }}
                         </td>
                     </tr>
                 </tbody>
