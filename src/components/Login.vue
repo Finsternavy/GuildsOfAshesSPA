4d6e617ac3954981906227bb90e432c3bb205b22
Christopher Finster <finsternavy@gmail.com>
2023-08-27 01:27:31 -0500

    connected to backend

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,16 +5,45 @@ import { useUserStore } from "../stores/userStore";
 
 const username = ref("");
 const password = ref("");
-
+const baseUrl = process.env.APIURL;
 const store = useUserStore();
 
-const doLogin = () => {
+const doLogin = async () => {
   // for now just login no matter what.
   const userStore = useUserStore;
-  store.setAuthenticated(true);
-  console.log("Authenticated: ", store.getAuthenticated());
-  console.log("User: ", store.getUser());
-  router.push("/guild/home");
+  console.log("baseURL: ", baseUrl);
+  const call = {
+    Username: username.value,
+    Password: password.value,
+  };
+  const response = await fetch("http://localhost:5229/Users/authenticate", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "GET, POST, DELETE, PUT",
+    },
+    body: JSON.stringify(call),
+  })
+    .then((response) => {
+      // console.log("appLogin", response);
+      if (response.ok) {
+        return response;
+      }
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+      return error;
+    });
+  returnValue = await response;
+
+  return returnValue;
+
+  // store.setAuthenticated(true);
+  // console.log("Authenticated: ", store.getAuthenticated());
+  // console.log("User: ", store.getUser());
+  // router.push("/guild/home");
 };
 </script>
 
@@ -31,7 +60,12 @@ const doLogin = () => {
     <div class="login uk-form uk-padding uk-width-large uk-align-center">
       <div class="uk-margin-bottom">
         <label for="username">Username</label>
-        <input id="username" type="text" class="goa-input uk-width-1-1 uk-text-large" />
+        <input
+          id="username"
+          type="text"
+          class="goa-input uk-width-1-1 uk-text-large"
+          v-model="username"
+        />
       </div>
       <div class="uk-margin-bottom">
         <label for="password">Password</label>
@@ -39,6 +73,7 @@ const doLogin = () => {
           id="password"
           type="password"
           class="goa-input uk-width-1-1 uk-text-large"
+          v-model="password"
         />
       </div>
       <button
