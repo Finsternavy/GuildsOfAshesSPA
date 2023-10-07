b6bad32ffa506fabc5ffa1086db09d79055214c0
Christopher Finster <finsternavy@gmail.com>
2023-10-07 17:16:34 -0500

    setup local backend connection

--- src/stores/apiStore.js
+++ src/stores/apiStore.js
@@ -3,8 +3,8 @@ import { defineStore } from 'pinia'
 export let useAPI = defineStore('api', {
 
   state: () => ({
-    baseURL: 'https://goabackend.azurewebsites.net/', // development deployment URL
-    // baseURL: 'http://localhost:5229/',    // local deployment URL
+    // baseURL: 'https://goabackend.azurewebsites.net/', // development deployment URL
+    baseURL: 'http://localhost:5229/',    // local deployment URL
   }),
 
   getters: {
